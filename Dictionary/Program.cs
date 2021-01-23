06d52f1d3eeb545c901b599a87328d4e10011e2d
Emre Aka <aka548548@gmail.com>
2021-01-23 20:47:10 +0300

    Dictionary Example

---
+++ Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<int, string> numberNames = new Dictionary<int, string>();
+            numberNames.Add(1, "One"); //adding a key/value using the Add() method
+            numberNames.Add(2, "Two");
+            numberNames.Add(3, "Three");
+            Console.WriteLine(numberNames[1]); //printing value of key 1.
+        }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string>();
+            cities.Add("Tekirdağ");
+            Console.WriteLine(cities.Count);
+
+            MyList<string> cities1 = new MyList<string>();
+            cities1.Add("Tekirdağ");
+        }
+    }
+}
+
+class MyList<T> //Generic class.
+{
+    public void Add(T item) 
+    {
+        
+    }
+
+}
